4c8aa59181445a00508ffe82336c27af40c4cdb3
lada66766 <lada66766@yandex.ru>
2022-12-11 13:47:17 +0500

    Update main.py

--- Tg_bot/main.py
+++ Tg_bot/main.py
@@ -11,10 +11,12 @@ def start(m, res=False):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=types.KeyboardButton("Картинка")
     markup.add(item1)
+    bot.send_message(m.chat.id, 'Нажми: \nКартинка  Если хочешь узнать какой из 50 Ёожыгов ты сегодня',reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if message.text.strip() == 'Картинка':
         photo = open('pic/' + random.choice(os.listdir('pic')), 'rb')
         bot.send_photo(message.from_user.id, photo)
+
 bot.polling(none_stop=True, interval=0)
